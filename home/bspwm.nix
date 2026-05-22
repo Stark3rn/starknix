b1df9db83aeac4312d796ac2a442dee31fe7257d
Stark3rn <lbastardnaminzo@gmail.com>
2026-05-22 20:35:17 +0200

    Update bspwm.nix

--- home/bspwm.nix
+++ home/bspwm.nix
@@ -1,16 +1,17 @@
 { config, pkgs, lib, ... }:
-
 {
-  home.activation.copyConfigs =
+  home.file.".config/bspwm/bspwmrc".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/etc/nixos/dotfiles/bspwm/bspwmrc";
+
+  home.file.".config/sxhkd/sxhkdrc".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/etc/nixos/dotfiles/sxhkd/sxhkdrc";
+
+  home.activation.chmodBspwmrc =
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      mkdir -p ~/.config/bspwm
-      mkdir -p ~/.config/sxhkd
-
-      cp -n /etc/nixos/dotfiles/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-      cp -n /etc/nixos/dotfiles/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-
       chmod +x ~/.config/bspwm/bspwmrc
     '';
 
-  home.stateVersion = "25.11";
+  home.stateVersion = "24.11";
 }
